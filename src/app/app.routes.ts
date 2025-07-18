44ecad673db3dc5b2aa14ecd6e1f3d18e4b4f900
Gustaalexandre <galexandre389@gmail.com>
2025-07-18 09:38:56 -0300

    Atualização de cadastro usuario junto com pessoa

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,12 +3,14 @@ import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { LoginComponent } from './pages/login/login.component';
 import { AddUsuarioComponent } from './pages/add-usuario/add-usuario.component';
+import { AddPessoaComponent } from './pages/add-pessoa/add-pessoa.component';
 
 
 
 export const routes: Routes = [
   { path: 'home', component: HomeComponent },
   { path: '', component: LoginComponent },
-  { path: 'add-usuario', component: AddUsuarioComponent }
+  { path: 'add-usuario', component: AddUsuarioComponent },
+  { path: 'add-pessoa', component: AddPessoaComponent }
 
 ];
---
+++ src/app/pages/add-pessoa/add-pessoa.component.ts
@@ -0,0 +1,117 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { UsuarioService } from '../../services/usuario.service';
+import { Router, ActivatedRoute, RouterModule } from '@angular/router';
+import { PessoaService } from '../../services/pessoa.service';
+import { Pessoa } from '../../models/pessoa';
+import { Usuario } from '../../models/usuario';
+
+@Component({
+  selector: 'add-pessoa',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  templateUrl: './add-pessoa.component.html',
+  styleUrl: './add-pessoa.component.css'
+})
+export class AddPessoaComponent {
+
+  formGroup: FormGroup;
+  mensagemErroLogin: string;
+  listaPessoas: Pessoa[] = [];
+
+  constructor(private formBuilder: FormBuilder, private service: UsuarioService, private route: ActivatedRoute, private router: Router, private pessoaService: PessoaService) {
+
+
+    this.formGroup = this.formBuilder.group({
+      id: [null],
+      nome: ['', Validators.required],
+      sexo: ['', Validators.required],
+      endereco: ['', Validators.required],
+      login: ['', Validators.required],
+      senha: ['', Validators.required],
+      nivelAcesso: ['', Validators.required],
+    });
+
+    this.mensagemErroLogin = "";
+  }
+
+  ngOnInit(): void {
+    this.carregarListaPessoa();
+  }
+
+  onSubmit(): void {
+    if (this.formGroup.valid) {
+      // Extrair dados da pessoa
+      const pessoaData = new Pessoa();
+
+      pessoaData.nome = this.formGroup.get('nome')?.value;
+      pessoaData.sexo = this.formGroup.get('sexo')?.value;
+      pessoaData.endereco = this.formGroup.get('endereco')?.value;
+
+
+      this.pessoaService.salvar(pessoaData).subscribe({
+        next: (pessoaSalva: Pessoa) => {
+          // Após salvar a pessoa, salvar o usuário com o id da pessoa
+          const usuarioData = new Usuario();
+
+          usuarioData.login = this.formGroup.get('login')?.value;
+          usuarioData.senha = this.formGroup.get('senha')?.value;
+          usuarioData.nivelAcesso = this.formGroup.get('nivelAcesso')?.value;
+          usuarioData.pessoaId = pessoaSalva.id; // Aqui é o vínculo
+
+
+          this.service.salvar(usuarioData).subscribe({
+            next: () => {
+              alert('Usuário criado com sucesso!');
+              this.formGroup.reset();
+              this.router.navigate(['/home']);
+            },
+            error: () => {
+              alert('Erro ao criar usuário.');
+            }
+          });
+        },
+        error: () => {
+          alert('Erro ao salvar pessoa.');
+        }
+      });
+    }
+  }
+
+
+
+  verificarLogin() {
+    const login = this.formGroup.get('login')?.value;
+    this.mensagemErroLogin = "";
+
+    this.service.verificarLogin(login).subscribe({
+      next: (existe: boolean) => {
+        if (existe) {
+          this.mensagemErroLogin = "Login já cadastrado.";
+          this.formGroup.get('login')?.setErrors({ loginDuplicado: true });
+        } else {
+          this.mensagemErroLogin = "";
+          // Limpa o erro de loginDuplicado, se existir
+          this.formGroup.get('login')?.setErrors(null);
+        }
+      },
+      error: err => {
+        this.mensagemErroLogin = "Erro ao validar o login";
+      }
+    });
+  }
+
+  carregarListaPessoa(): void {
+    this.pessoaService.listar().subscribe({
+      next: (retornoJson) => {
+        this.listaPessoas = retornoJson;
+      },
+      error: () => {
+        alert('Erro ao carregar a lista de pessoas.');
+      }
+    });
+  }
+
+
+}
